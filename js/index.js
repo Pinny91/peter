d49b6f4c4f793b3be802bfb499fc0c77d7628050
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2017-04-02 13:50:47 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,106 @@
+
+/*MENU FUNCTION*/
+jQuery(document).ready(function(){
+	if (jQuery(window).width() < 1165) {
+		jQuery("#small-menu").on('click', function() {
+			event.preventDefault();
+			jQuery("#small-menu").toggleClass("active");
+			jQuery("#nav-list ul").toggleClass("active");
+		});
+		jQuery('.nav-link').on('click' ,function(){
+			jQuery("#nav-list ul").addClass("active");
+			console.log("Test");
+		});
+	}
+	else {
+		jQuery("#nav-list ul").removeClass("active");
+	}
+	jQuery(window).resize(function() {
+		if (jQuery(window).width() < 1165) {
+			jQuery("#nav-list ul").addClass("active");
+			console.log("size: " + jQuery(window).width());
+			
+			//When pressed on the menu icon -> open the menu, or close
+			jQuery("#small-menu").on('click', function() {
+				jQuery("#nav-list ul").toggleClass("active");
+				console.log("Open & toe");
+			});
+			
+			//When you clicked an element from the menu, it should close
+			jQuery('.nav-link').on('click' ,function(){
+				jQuery("#nav-list ul").addClass("active");
+				console.log("Test");
+			});
+		}
+		else {
+			jQuery("#nav-list ul").removeClass("active");
+		}
+	});	
+});
+
+	/*PROJECTS FUNCTION*/
+	$(document).ready(function() {
+			$('.project-list > li > a').click(function(){
+			event.preventDefault();//stop browser to take action for clicked anchor
+						
+			//get displaying tab content jQuery selector
+			var active_tab_selector = $('.project-list > li.pr-active > a').attr('href');					
+						
+			//find actived navigation and remove 'active' css
+			var actived_nav = $('.project-list > li.pr-active');
+			actived_nav.removeClass('pr-active');
+						
+			//add 'active' css into clicked navigation
+			$(this).parents('li').addClass('pr-active');
+						
+			//hide displaying tab content
+			$(active_tab_selector).removeClass('pr-active');
+			$(active_tab_selector).addClass('pr-hide');
+						
+			//show target tab content
+			var target_tab_selector = $(this).attr('href');
+			$(target_tab_selector).removeClass('pr-hide');
+			$(target_tab_selector).addClass('pr-active');
+			 });
+	});
+
+$(".btn").mouseup(function(){
+    $(this).blur();
+})
+
+
+function miniGlasChange(){
+	var image = document.getElementById("glas");
+	var dropd = document.getElementById("glasList");
+	image.src = dropd.value;	
+};
+
+function miniBumperChange(){
+	var image = document.getElementById("bumper");
+	var dropd = document.getElementById("bumperList");
+	image.src = dropd.value;	
+};
+
+function miniPinkerChange(){
+	var image = document.getElementById("pinker");
+	var dropd = document.getElementById("pinkerList");
+	image.src = dropd.value;	
+};
+
+function miniSpiegelChange(){
+	var image = document.getElementById("spiegels");
+	var dropd = document.getElementById("spiegelsList");
+	image.src = dropd.value;	
+};
+
+function miniTankDopChange(){
+	var image = document.getElementById("tankDop");
+	var dropd = document.getElementById("tankDopList");
+	image.src = dropd.value;	
+};
+
+function miniSpoilerChange(){
+	var image = document.getElementById("spoiler");
+	var dropd = document.getElementById("spoilerList");
+	image.src = dropd.value;	
+};
