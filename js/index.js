4c1bfad9ece44cf4cacb02cf0d29b9f5052dd970
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2017-05-23 15:52:38 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -402,9 +402,35 @@ function miniMotorKapChange(){
 	}		
 };
 
+var partList = document.getElementById("partList");
 
 
 
+// mailing
+
+var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: '//formspree.io/pinnewaert@gmail.com',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: "json",
+		contentType: "application/json; charset=utf-8",
+
+		beforeSend: function() {
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+		},
+		success: function(data) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+		},
+		error: function(err) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+		}
+	});
+});
 
 
 
